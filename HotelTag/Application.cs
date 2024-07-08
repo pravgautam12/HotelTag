0bbe47e2cc50c5a32c3ea14a009c0c3d8ea75f8d
pravgautam12 <pravingautam22@outlook.com>
2024-07-08 11:25:05 -0500

    Add project files.

---
+++ HotelTag/Application.cs
@@ -0,0 +1,188 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using Autodesk.Revit.UI.Events;
+using System.Xml.Linq;
+using Autodesk.Revit.DB.Electrical;
+
+namespace HotelTag
+{
+    public class Application : IExternalApplication
+    {
+
+        private static DateTime lastModificationTime = DateTime.MinValue;
+        private static readonly TimeSpan modificationInterval = TimeSpan.FromSeconds(1);
+        public Result OnStartup(UIControlledApplication application)
+        {
+
+            CommentUpdater commentUpdater = new CommentUpdater(application.ActiveAddInId);
+            UpdaterRegistry.RegisterUpdater(commentUpdater, true);
+
+            //Add triggers to the filter
+            ElementClassFilter familyInstanceFilter = new ElementClassFilter(typeof(FamilyInstance));
+            ElementClassFilter electricalSystemFilter = new ElementClassFilter(typeof(ElectricalSystem));
+            LogicalOrFilter combinedFilter = new LogicalOrFilter(electricalSystemFilter, familyInstanceFilter);
+            UpdaterRegistry.AddTrigger(commentUpdater.GetUpdaterId(), combinedFilter, Element.GetChangeTypeElementAddition());
+            UpdaterRegistry.AddTrigger(commentUpdater.GetUpdaterId(), combinedFilter, Element.GetChangeTypeAny());
+
+
+            String tabName = "HotelTag";
+            application.CreateRibbonTab(tabName);
+
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "HotelTag");
+            ribbonPanel.Name = "HotelTag";
+
+            PushButtonData b1Data = new PushButtonData("Add tag", "Add tag", Assembly.GetExecutingAssembly().Location, "HotelTag.jBoxTag");
+            ribbonPanel.AddItem(b1Data);
+
+
+            //application.Idling += OnIdling;
+
+            return Result.Succeeded;
+        }
+
+
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            // Unregister the updater
+            UpdaterRegistry.UnregisterUpdater(new CommentUpdater(application.ActiveAddInId).GetUpdaterId());
+
+            application.ControlledApplication.DocumentOpened -= OnDocumentOpened;
+            return Result.Succeeded;
+        }
+
+        private void OnDocumentOpened(object sender, DocumentOpenedEventArgs e)
+        {
+
+            TaskDialog.Show("YOU TRIGGERED?", "Don't worry! It's just IUpdater");
+
+        }
+
+
+    }
+
+    public class CommentUpdater : IUpdater
+    {
+        public static UpdaterId _updaterId;
+        public static int _caseSwitch = 1;
+        
+
+
+
+        public CommentUpdater(AddInId id)
+        {
+            _updaterId = new UpdaterId(id, new Guid("77A928B1-3AEF-4602-AAE5-2C4054437411"));
+        }
+
+        public void Execute(UpdaterData data)
+        {
+            bool elementProcessed = false;
+
+            switch (_caseSwitch)
+            {
+
+                case 1:
+
+                    Document doc = data.GetDocument();
+
+                    //UpdaterRegistry.DisableUpdater(_updaterId);
+                    ICollection<ElementId> modifiedElements = data.GetModifiedElementIds();
+
+                    if (modifiedElements.Count > 0)
+                    {
+                        foreach (ElementId elementId in modifiedElements)
+                        {
+                            Element element = doc.GetElement(elementId);
+
+                            //if (element != null && element.Name == "xx")
+
+                            if (element is FamilyInstance) {
+                                if (element != null && (element as FamilyInstance).Symbol.Family.Name == "se_Hotel Unit")
+                                {
+                                    string updatingTag = "";
+                                    int n = 0;
+                                    jBoxTag.EditCommentsOnly(element as FamilyInstance, updatingTag, n, doc);
+                                    elementProcessed = true;
+                                }
+                            }
+
+                            else
+                            {
+                                try
+                                {
+                                    ElectricalSystem elecSys = element as ElectricalSystem;
+                                    ElementSet elementSet = elecSys.Elements;
+                                    foreach (Element elem in elementSet)
+                                    {
+                                        FamilyInstance famins = elem as FamilyInstance;
+                                        if (famins != null && famins.Symbol.Family.Name == "se_Hotel Unit")
+                                        {
+                                            string updatingTag = "";
+                                            int n = 0;
+                                            jBoxTag.EditCommentsOnly(famins, updatingTag, n, doc);
+                                            elementProcessed = true;
+                                        }
+                                    }
+
+
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                        }
+
+
+                        //_caseSwitch = 2;
+                    }
+
+                    //else
+                    //{
+                    //    _caseSwitch = 1;
+                    //}
+                    //break;
+
+                    if (elementProcessed)
+                    {
+                        _caseSwitch = 2;
+                    }
+                    break;
+
+                case 2:
+                    _caseSwitch = 1;
+                    break;
+
+            }
+
+            //UpdaterRegistry.EnableUpdater(_updaterId);
+
+        }
+
+        public string GetAdditionalInformation()
+        {
+            return "Updates";
+        }
+        public ChangePriority GetChangePriority()
+        {
+            return ChangePriority.MEPFixtures;
+        }
+
+        public UpdaterId GetUpdaterId()
+        {
+            return _updaterId;
+        }
+
+        public String GetUpdaterName()
+        {
+            return "suppy puppy";
+        }
+
+
+    }
+}
+
---
+++ HotelTag/jBoxTag.cs
@@ -0,0 +1,145 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace HotelTag
+{
+
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class jBoxTag : IExternalCommand
+    {
+        string jBoxName = "xx";
+        public Result Execute(ExternalCommandData cmdData, ref string message, ElementSet Elements)
+        {
+            UIApplication uiapp = cmdData.Application as UIApplication;
+            Document doc = uiapp.ActiveUIDocument.Document;
+    
+            IList<ViewPlan> views = new FilteredElementCollector(doc).OfClass(typeof(ViewPlan)).WhereElementIsNotElementType().Cast<ViewPlan>().
+            Where(v => v.IsTemplate == false && v.ViewType == ViewType.FloorPlan && v.Name.Contains("POWER")).ToList();
+            //List<FamilyInstance> hotelJBoxes = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_ElectricalFixtures).WhereElementIsNotElementType()
+            //.OfType<FamilyInstance>().Where(e => e.Symbol.Family.Name == "se_Hotel Unit" && e.Symbol.Name == jBoxName).ToList<FamilyInstance>();
+
+            List<FamilyInstance> hotelJBoxes = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_ElectricalFixtures).WhereElementIsNotElementType()
+            .OfType<FamilyInstance>().Where(e => e.Symbol.Family.Name == "se_Hotel Unit").ToList<FamilyInstance>();
+            IList<Element> tags = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_ElectricalFixtureTags).WhereElementIsElementType().Where(e => e.Name == "Guestroom Feeder (3 Ckts)" || e.Name == "Guestroom Feeder (7 Ckts)").ToList();
+
+            Transaction trans = new Transaction(doc, "editing comments and adding tag");
+            trans.Start();
+
+            int n = 0;
+            string updatingTag = "";
+            foreach (ViewPlan v in views)
+            {
+                bool x = EditComments(v, updatingTag, doc, n);
+            }
+
+            trans.Commit();
+
+            return Result.Succeeded;
+        }
+
+       
+        public bool EditComments(ViewPlan v, string updatingTag, Document doc, int n)
+        {
+            List<FamilyInstance> hotelJBoxes = new FilteredElementCollector(doc, v.Id).OfCategory(BuiltInCategory.OST_ElectricalFixtures).WhereElementIsNotElementType()
+            .OfType<FamilyInstance>().Where(e => e.Symbol.Family.Name == "se_Hotel Unit" && e.Symbol.Name == jBoxName).ToList<FamilyInstance>();
+            
+            IList<Element> tags = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_ElectricalFixtureTags).WhereElementIsElementType().Where(e => e.Name == "Guestroom Feeder (3 Ckts)" || e.Name == "Guestroom Feeder (7 Ckts)").ToList();
+
+            
+            if (hotelJBoxes.Count > 0)
+
+            {
+                foreach (FamilyInstance hotelJBox in hotelJBoxes)
+                {
+                    int a = EditCommentsOnly(hotelJBox, updatingTag, n, doc);
+                    //if (n <= 3 && a != true)
+                    if (a > 0 && a <= 3)
+                    {
+                        Element tagType = tags.FirstOrDefault(e => e.Name == "Guestroom Feeder (3 Ckts)") as Element;
+                        IndependentTag.Create(doc, tagType.Id, v.Id, new Reference(hotelJBox), false, TagOrientation.Horizontal, (hotelJBox.Location as LocationPoint).Point);
+                        
+                    }
+                    //if ( 3 < n && n <= 7 && a != true)
+                    if (a > 3)
+                    {
+                        Element tagType = tags.FirstOrDefault(e => e.Name == "Guestroom Feeder (7 Ckts)") as Element;
+                        IndependentTag.Create(doc, tagType.Id, v.Id, new Reference(hotelJBox), false, TagOrientation.Horizontal, (hotelJBox.Location as LocationPoint).Point);
+                        
+                    }
+                }
+
+                if (n > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static int EditCommentsOnly(FamilyInstance hotelJBox, string updatingTag, int n, Document doc)
+        {
+            MEPModel jBoxMEPModel = hotelJBox.MEPModel;
+            ConnectorManager c = jBoxMEPModel.ConnectorManager;
+            ConnectorSet connectors = c.Connectors;
+            var sortedConnectors = connectors.Cast<Connector>().Select(connector => new
+            {
+                Connector = connector,
+                Order = int.TryParse(connector.Description.Split(')')[0].Trim(), out int order) ? order : int.MaxValue
+            }).OrderBy(x => x.Order).Select(x => x.Connector).ToList();
+            updatingTag = "";
+            foreach (Connector connector in sortedConnectors)
+            {
+                ConnectorSet connectorReferences = connector.AllRefs;
+                if (connectorReferences != null)
+                {
+                    ElectricalSystem firstReferenceElecSys = null;
+                    foreach (Connector reference in connectorReferences)
+                    {
+                        firstReferenceElecSys = reference.Owner as ElectricalSystem;
+                        break;
+                    }
+                    if (firstReferenceElecSys != null)
+                    {
+                        string circuitNumber = firstReferenceElecSys.PanelName + " - " + firstReferenceElecSys.CircuitNumber;
+
+
+                        if (connector == sortedConnectors[0])
+                        {
+                            updatingTag += circuitNumber;
+                        }
+                        else
+                        {
+                            updatingTag = updatingTag + " " + circuitNumber;
+                        }
+
+                        
+                        hotelJBox.LookupParameter("SE_E_COMMENTS").Set(updatingTag);
+                        n++;
+                    }
+                }
+            }
+            
+            FilteredElementCollector tagCollector = new FilteredElementCollector(doc).OfClass(typeof(IndependentTag)).OfCategory(BuiltInCategory.OST_ElectricalFixtureTags);
+            foreach (IndependentTag tag in tagCollector)
+            {
+                ICollection<LinkElementId> taggedElementIds = tag.GetTaggedElementIds();
+                foreach (LinkElementId linkElementId in taggedElementIds)
+                {
+                    if (linkElementId.HostElementId == hotelJBox.Id)
+                    {
+                        return 0;
+                    }
+                }
+            }
+            return n;
+        }
+    }
+}
